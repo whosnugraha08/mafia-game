4b541586ad378c3d8351c9ec95fec00922fdb1a1
whosnugraha08 <106441305+whosnugraha08@users.noreply.github.com>
2026-03-18 00:09:06 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`\n🎭 Deduksi Game Server running → http://localhost:${PORT}\n`);
 });
